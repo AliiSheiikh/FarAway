06e38615e8f2df55607c5de2c45bc3bb36403351
Ali Sheikh <l_lawliet786@hotmail.com>
2025-07-26 22:05:32 -0400

    added list rendering with item prop component

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,3 +1,20 @@
+import Item from "./Item";
+
 export default function PackingList() {
-  return <div className="list">List</div>;
+  const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: true },
+    { id: 3, description: "Shirts", quantity: 2, packed: false },
+    { id: 4, description: "Pants", quantity: 2, packed: true },
+  ];
+
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item itemObj={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
