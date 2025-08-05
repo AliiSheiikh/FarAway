3b4b985227ebc474733abef3208c5d6d11453a04
Ali Sheikh <l_lawliet786@hotmail.com>
2025-08-05 19:16:39 -0400

    delete item functionality implemented

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,10 @@
-export default function Item({ itemObj }) {
+export default function Item({ itemObj, onDeleteItem }) {
   return (
     <li>
       <span style={itemObj.packed ? { textDecoration: "line-through" } : {}}>
         {itemObj.quantity} {itemObj.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(itemObj.id)}>❌</button>
     </li>
   );
 }
--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,18 +1,11 @@
 import Item from "./Item";
 
-export default function PackingList({ items }) {
-  const initialItems = [
-    { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: true },
-    { id: 3, description: "Shirts", quantity: 2, packed: false },
-    { id: 4, description: "Pants", quantity: 2, packed: true },
-  ];
-
+export default function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item itemObj={item} key={item.id} />
+          <Item itemObj={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
